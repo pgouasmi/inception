90a6aff3cb8d7b491ff15f59747b2cd7a4ce5475
pgouasmi <pgouasmi@student.42lyon.fr>
2024-03-27 14:43:55 +0100

    y

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -3,14 +3,14 @@
 mv var/www/wordpress/* /var/www/html/
 
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-echo mv ok
-wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html/
-echo "1 ok"
-wp config set DB_USER $MYSQL_USER --allow-root --path=/var/www/html/
-echo "2 ok"
-wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html/
-echo "3 ok"
+# echo mv ok
+wp config set DB_NAME $SQL_DATABASE --allow-root --path=/var/www/html/
+# echo "1 ok"
+wp config set DB_USER $SQL_USER --allow-root --path=/var/www/html/
+# echo "2 ok"
+wp config set DB_PASSWORD $SQL_PASSWORD --allow-root --path=/var/www/html/
+# echo "3 ok"
 wp config set DB_HOST mariadb --allow-root --path=/var/www/html/
-echo "4 ok"
+# echo "4 ok"
 
-./usr/sbin/php-fpm7.4 -F
+# ./usr/sbin/php-fpm7.4 -F
