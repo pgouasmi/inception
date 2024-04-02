cca5b2b6fc065a3cc87d43bbe9c3f6f07f6dc5c8
Pascal Gouasmi <pgouasmi@z1r2p5.42lyon.fr>
2024-04-02 12:48:53 +0200

    script nice and tidy

--- srcs/requirements/wordpress/tools/install.sh
+++ srcs/requirements/wordpress/tools/install.sh
@@ -4,16 +4,30 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
 
 	wp-cli.phar core download --path=/var/www/wordpress --allow-root
 
-	chown -R www-data:www-data /var/www/wordpress
 
-	wp-cli.phar config create --dbname=${WORDPRESS_DATABASE} --dbuser=${WORDPRESS_DATABASE_USER} --dbpass=${WORDPRESS_DATABASE_USER_PASSWORD} --dbhost=mariadb:3306 --path=/var/www/wordpress --allow-root
+	wp-cli.phar config create --dbname=${WORDPRESS_DATABASE} \
+	                          --dbuser=${WORDPRESS_DATABASE_USER} \
+	                          --dbpass=${WORDPRESS_DATABASE_USER_PASSWORD} \
+	                          --dbhost=mariadb:3306 \
+	                          --path=/var/www/wordpress \
+	                          --allow-root
 
-	wp-cli.phar core install --url="${WORDPRESS_WEBSITE_URL}" --title="${WORDPRESS_WEBSITE_TITLE}" --admin_user="${WORDPRESS_WEBSITE_ADMIN}" --admin_password="${WORDPRESS_WEBSITE_ADMIN_PASSWORD}" --admin_email=${WORDPRESS_WEBSITE_ADMIN_EMAIL} --allow-root --path=/var/www/wordpress
+	wp-cli.phar core install --url="${WORDPRESS_WEBSITE_URL}" \
+	                         --title="${WORDPRESS_WEBSITE_TITLE}" \
+	                         --admin_user="${WORDPRESS_WEBSITE_ADMIN}" \
+	                         --admin_password="${WORDPRESS_WEBSITE_ADMIN_PASSWORD}" \
+	                         --admin_email=${WORDPRESS_WEBSITE_ADMIN_EMAIL} \
+	                         --path=/var/www/wordpress \
+	                         --allow-root
 
-	wp-cli.phar user create ${WORDPRESS_WEBSITE_USER} ${WORDPRESS_WEBSITE_USER_EMAIL} --user_pass=${WORDPRESS_WEBSITE_USER_PASSWORD} --role=editor --allow-root --path=/var/www/wordpress
+	wp-cli.phar user create ${WORDPRESS_WEBSITE_USER} ${WORDPRESS_WEBSITE_USER_EMAIL} \
+	                        --user_pass=${WORDPRESS_WEBSITE_USER_PASSWORD} \
+	                        --role=editor \
+	                        --path=/var/www/wordpress \
+	                        --allow-root
 
 fi
 
 mkdir -p /run/php
-
+chown -R www-data:www-data /var/www/wordpress
 php-fpm7.4 -F
